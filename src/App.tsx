9c74849ad50829912beb8952c1ac4ea3fe361998
seanersbutt36 <seangrabowski819@gmail.com>
2026-09-09 16:10:00 -0400

    Fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
     const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
     "/"]
 
-    let password1El = document.getElementById("password1-el")
-    let password2El = document.getElementById("password2-el")
+    let password1El = document.getElementById("password1-el")!
+    let password2El = document.getElementById("password2-el")!
     let characterLength = 15;
 
     function generatePasswords() {
