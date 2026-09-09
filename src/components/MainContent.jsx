ceac90ec4294076d9a0979f438e34c70c562c6ed
seanersbutt36 <seangrabowski819@gmail.com>
2026-09-09 15:05:03 -0400

    Init and apply with React framework
    Apply the React framework into the application.

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+    return (
+        <>
+            <h1 id="generate-a-header">Generate a</h1>
+            <h1>random password</h1>
+            <p>Never use an insecure password again.</p>
+        </>
+    )
+}
+
+export default MainContent
